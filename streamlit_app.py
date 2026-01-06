d4d278e0b4d8b0de1603858c9c08d3c17eb8334f
Aichem9 <ssactung@gmail.com>
2026-01-06 16:29:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(page_title="추정 분할 점수 생성기", layout="wide")
 st.title("추정 분할 점수 생성기 (고정 규칙 버전) 서울 인창고 AIchem 제작")
 
-st.info("먼저 **사이드바에서 A~E 수준별 정답률(%)**을 설정한 뒤, **문항 정보표 엑셀 파일(.xls저장)**을 업로드하세요.", icon="ℹ️")
+st.info("먼저 **사이드바에서 A~E 수준별 정답률(%)**을 설정한 뒤, **문항 정보표 엑셀 파일(.xls data형식으로 저장)**을 업로드하세요.", icon="ℹ️")
 
 with st.expander("규칙 요약 및 사용 순서", expanded=False):
     st.markdown("""
